cb89362a3bb3d1d73f6cbcf90282c93d3e5fdc53
Gautier <gautier.blandin@gmail.com>
2024-06-08 12:52:23 +0200

    feat(article): add json formatting

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -1,6 +1,7 @@
 import 'prismjs/themes/prism-okaidia.css';
 import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-json';
 import 'prismjs/plugins/toolbar/prism-toolbar.min.css';
 import 'prismjs/plugins/toolbar/prism-toolbar.min';
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min';
