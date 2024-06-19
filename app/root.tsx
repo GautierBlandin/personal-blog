c7e1804923e6f24adc6344ea139338121a326fec
Gautier <gautier.blandin@gmail.com>
2024-06-19 20:42:22 +0200

    feat(root-favicon): add root favicon

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,10 @@ export const links: LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap',
   },
+  {
+    rel: 'icon',
+    href: '/favicon.ico',
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
